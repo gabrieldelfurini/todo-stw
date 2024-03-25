427c199670fbe831aaee84ece577756b84f8a7f1
Gabriel de Lima Furini <bielfurini2016@gmail.com>
2024-03-25 16:33:46 -0300

    Adiciona autenticação

--- todo-laravel/database/seeders/TodoSeeder.php
+++ todo-laravel/database/seeders/TodoSeeder.php
@@ -16,3 +16,4 @@ class TodoSeeder extends Seeder
         //
     }
 }
+
--- todo-laravel/routes/api.php
+++ todo-laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TodosController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
+
+
 
 Route::get('/todos', [TodosController::class, 'index']);
 Route::post('/todos', [TodosController::class, 'store']);
